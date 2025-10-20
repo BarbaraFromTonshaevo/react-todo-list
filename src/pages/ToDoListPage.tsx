ca7c9e25dba489967bfa22ba09169f879c4adf08
Elkina Barbara <elkinavarvara1999@gmail.com>
2025-10-20 16:59:08 +0300

    day 2^ relocation of layout

---
+++ src/pages/ToDoListPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Header } from '../components/Header/Header'
+import { Form } from '../components/Form/Form'
+import { ToDoList } from '../components/ToDoList/ToDoList'
+
+export const ToDoListPage = () => {
+	return (
+		<>
+			<Header />
+			<Form />
+			<ToDoList />
+		</>
+	)
+}
