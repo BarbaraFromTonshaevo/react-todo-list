efca10e026f71390479c8dbae9f3c96aa571bb94
Elkina Barbara <elkinavarvara1999@gmail.com>
2025-10-25 23:28:34 +0300

    Day 6: Styled-components

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
-import './Form.scss'
+import {
+	FormWrapper,
+	FormBlock,
+	FormLabel,
+	FormField,
+	FormControl,
+} from './Form.styled'
+import plusIcon from '../../assets/images/plus.png'
 
 export const Form = (props: { createNewToDo: Function }) => {
 	const [text, setText] = useState<string>('')
 
-	const formSubmit = (event: React.SyntheticEvent) => {
-        event.preventDefault()
+	const formSubmit = () => {
 		if (text) {
 			props.createNewToDo(text)
 			setText('')
@@ -13,13 +19,17 @@ export const Form = (props: { createNewToDo: Function }) => {
 	}
 
 	return (
-		<div className='form-wrapper'>
-			<form action='#' onSubmit={formSubmit}>
-				<label>
-					<input value={text} type='text' onChange={(e) => setText(e.target.value) } />
-					<button></button>
-				</label>
-			</form>
-		</div>
+		<FormWrapper>
+			<FormBlock action='#' onSubmit={formSubmit}>
+				<FormLabel>
+					<FormField
+						value={text}
+						type='text'
+						onChange={e => setText(e.target.value)}
+					/>
+					<FormControl icon={plusIcon} />
+				</FormLabel>
+			</FormBlock>
+		</FormWrapper>
 	)
 }
